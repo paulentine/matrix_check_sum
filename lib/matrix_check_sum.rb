d668df14edaa117560ffc9538b3dbdd0391fa703
Pauline Sauget <paulinevsauget@gmail.com>
2019-05-05 21:13:40 -0700

    Check sum of rows & columns of a given matrix

--- lib/matrix_check_sum.rb
+++ lib/matrix_check_sum.rb
@@ -3,8 +3,22 @@
 # of numbers in row i is the same as the sum of numbers in column i for i = 0 to row.length-1
 # If this is the case, return true. Otherwise, return false.
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n * n) = O(n^2), where n is the length of each row & column
+# Space complexity: O(1), since we only need to allocate a constant space for our variables
+
 def matrix_check_sum(matrix)
-  raise NotImplementedError
+  i = 0
+  j = 0
+  rows_sum = 0
+  cols_sum = 0
+  while matrix[i]
+    while matrix[j]
+      rows_sum += matrix[i][j]
+      cols_sum += matrix[j][i]
+      j += 1
+    end
+    return false if rows_sum != cols_sum
+    i += 1
+  end
+  return true
 end
